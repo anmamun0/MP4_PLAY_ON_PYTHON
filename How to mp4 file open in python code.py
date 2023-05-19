347474195d27705c3d0edc21d5f124f444c9c933
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2023-05-20 01:32:31 +0600

    Update How to mp4 file open in python code.py

--- How to mp4 file open in python code.py
+++ How to mp4 file open in python code.py
@@ -1,1 +1,11 @@
+# You need to install this module 
+# pip install pygame
+# pip install moviepy
 
+import pygame
+from moviepy.editor import *
+
+clip = VideoFileClip('video_file.mp4')
+clip.preview()
+ 
+pygame.quit()
